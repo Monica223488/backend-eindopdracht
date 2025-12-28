4b432c4374425a47f08545a26976481e910da803
Monica Moorlag <monicamoorlag@live.nl>
2025-12-28 20:03:03 +0100

    eerst opzet

---
+++ src/test/java/com/eindopdracht_backend/demo/EindopdrachtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eindopdracht_backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
